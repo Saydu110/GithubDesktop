e4ebf3a912e4a221d64bda5e546285a2362267fd
Saydullo_0208 <156135658+Saydu110@users.noreply.github.com>
2024-04-29 17:00:09 +0500

    update
    While

--- C# masalalar/ConsoleApp1/ConsoleApp1/Program.cs
+++ C# masalalar/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,82 @@
-﻿
+﻿using static System.Console;
+
+class Programm
+{
+    
+    static void Main(string[] args)
+    {
+        /*Write("start : ");
+        int s = int.Parse(ReadLine()!);
+        Write("finish : ");
+        int f = int.Parse(ReadLine()!);
+        while (s<=f)
+        {
+            WriteLine(s);
+            s++;
+        }*/
+        
+        /*Write("start : ");
+        int s = int.Parse(ReadLine()!);
+        Write("finish : ");
+        int f = int.Parse(ReadLine()!);
+        while (f<=s)
+        {
+            WriteLine(s);
+            f--;
+        }*/
+        
+        /*Write("start : ");
+        int s = int.Parse(ReadLine()!);
+        Write("finish : ");
+        int f = int.Parse(ReadLine()!);
+        if (s > f)
+        {
+            while (f<= s)
+            {
+                WriteLine(s);
+                s--;
+            }
+        }
+        else
+        {
+            while (s<=f)
+            {
+                WriteLine(s);
+                s++;
+            }
+        }*/
+
+        
+        
+            
+            /*int s = 1;
+            int count = 0;
+            int yigindi = 0;
+            while (s!=0)
+            {
+                Write("start : "); 
+                s = int.Parse(ReadLine()!);
+                yigindi+= s;
+                count++;
+            }
+            WriteLine($"siz {count} ta son kiritdingiz!\nyig'indisi {yigindi} ga teng");*/
+            
+            Random r = new Random();
+
+            Write("n : ");
+            int n = int.Parse(ReadLine()!);
+            int s = 1;
+            int yigindi = 0;
+            while (s<=n)
+            {
+                
+                int x = r.Next(1, 10);
+                WriteLine(x);
+                yigindi += x;
+                s++;
+            }
+            WriteLine(yigindi);
+
+
+    }
+}
--- C# masalalar/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ C# masalalar/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05631bb0e67605e46265f6f22b322fe587d0af02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920cb1f9d76530e913571adb56ededa19f4deaf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
